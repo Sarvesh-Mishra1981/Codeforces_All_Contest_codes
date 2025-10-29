f3ad63b12a1b8ea2b7b402f09ffd1bcf031d1c63
Sarvesh Mishra <sarveshmishra1981@Sarveshs-MacBook-Air.local>
2025-10-29 09:51:30 +0530

    29 OCT 2025

---
+++ Codeforces/B_Your_Name.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <string>
+#include <algorithm>
+#include <numeric>
+#include <cmath>
+#include <climits>
+#include <iomanip>
+#include <utility>
+#include<unordered_set>
+#include<unistd.h>
+#include<unordered_map>
+using namespace std;
+#define ll  long long
+#define ld  long double
+#define vi  vector<ll>
+#define pi  pair<ll,ll>
+#define vpi vector<pi>
+#define pb  push_back
+#define ff  first
+#define ss  second
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) ((ll)(x).size())
+#define fi(i,s,e) for(ll i=s;i<e;i++)
+#define fd(i,s,e) for(ll i=s;i>e;i--)
+#define tc  ll t;cin>>t;while(t--)
+#define in(v) for(auto &x:v) cin>>x
+#define out(v) for(auto &x:v) cout<<x<<" "
+#define inp(v) for(auto &x:v) cin>>x.ff>>x.ss
+#define Y cout<<"YES\n"
+#define N cout<<"NO\n"
+#define nl cout<<'\n'
+#define mn3(a,b,c) min(a,min(b,c))
+#define mx3(a,b,c) max(a,max(b,c))
+#define hi(v) *max_element(all(v))
+#define lo(v) *min_element(all(v))
+#define dbg(x) cerr<<#x<<"="<<x<<'\n'
+#define fast ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+const ll MOD = 1e9+7;
+const ll INF = LLONG_MAX;
+const int Na = 2e5+5;
+
+// -----------------------------------Lets Do IT---------------------------------------------------------------
+
+
+int main() {
+    fast;
+    tc {
+        ll n; cin>>n;
+        string a,b;
+        cin>>a>>b;
+        vector<int> freq(26, 0); 
+        for(int i=0; i<n;i++) {
+            freq[a[i]-'a']++;
+            freq[b[i]-'a']--;
+        }
+
+        bool ok =true;
+        for (int x :freq) {
+            if (x!=0) {
+                ok = false;
+                break;
+            }
+        }
+        if (ok) Y;
+        else N;
+    }
+}
