361522d0da64d942eb15c23a48865b79d8d78c1c
Sarvesh Mishra <191090264+Sarvesh-Mishra1981@users.noreply.github.com>
2025-12-04 14:50:34 +0530

    4 DEC 2025

--- Codeforces/C_Paint_the_Array.cpp
+++ Codeforces/C_Paint_the_Array.cpp
@@ -12,82 +12,104 @@
 #include <climits>
 #include <iomanip>
 #include <utility>
+#include <unordered_set>
+#include <unistd.h>
+#include <unordered_map>
 using namespace std;
-#define ll  long long
-#define ld  long double
-#define vi  vector<ll>
-#define pi  pair<ll,ll>
+#define ll long long
+#define ld long double
+#define vi vector<ll>
+#define pi pair<ll, ll>
 #define vpi vector<pi>
-#define pb  push_back
-#define ff  first
-#define ss  second
+#define pb push_back
+#define ff first
+#define ss second
 #define all(x) (x).begin(), (x).end()
 #define rall(x) (x).rbegin(), (x).rend()
 #define sz(x) ((ll)(x).size())
-#define fi(i,s,e) for(ll i=s;i<e;i++)
-#define fd(i,s,e) for(ll i=s;i>e;i--)
-#define tc  ll t;cin>>t;while(t--)
-#define in(v) for(auto &x:v) cin>>x
-#define out(v) for(auto &x:v) cout<<x<<" "
-#define inp(v) for(auto &x:v) cin>>x.ff>>x.ss
-#define Y cout<<"YES\n"
-#define N cout<<"NO\n"
-#define nl cout<<'\n'
-#define mn3(a,b,c) min(a,min(b,c))
-#define mx3(a,b,c) max(a,max(b,c))
+#define fi(i, s, e) for (ll i = s; i < e; i++)
+#define fd(i, s, e) for (ll i = s; i > e; i--)
+#define tc    \
+    ll t;     \
+    cin >> t; \
+    while (t--)
+#define in(v)         \
+    for (auto &x : v) \
+    cin >> x
+#define out(v)        \
+    for (auto &x : v) \
+    cout << x << " "
+#define inp(v)        \
+    for (auto &x : v) \
+    cin >> x.ff >> x.ss
+#define Y cout << "YES\n"
+#define N cout << "NO\n"
+#define nl cout << '\n'
+#define mn3(a, b, c) min(a, min(b, c))
+#define mx3(a, b, c) max(a, max(b, c))
 #define hi(v) *max_element(all(v))
 #define lo(v) *min_element(all(v))
-#define dbg(x) cerr<<#x<<"="<<x<<'\n'
-#define fast ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-const ll MOD = 1e9+7;
+#define dbg(x) cerr << #x << "=" << x << '\n'
+#define fast                 \
+    ios::sync_with_stdio(0); \
+    cin.tie(0);              \
+    cout.tie(0);
+const ll MOD = 1e9 + 7;
 const ll INF = LLONG_MAX;
-const int Na = 2e5+5;
+const int Na = 2e5 + 5;
 
 // -----------------------------------Lets Do IT---------------------------------------------------------------
-bool issame(vector<bool>& a,ll n){
-    fi(i,0,n-1){
-        if(a[i]==a[i+1]) return false; // must alternate
-    }
-    return true;
+ll gcd(ll a, ll b)
+{
+    if (b == 0)
+        return a;
+   return gcd(b, a % b);
 }
 
-bool check(ll d, vi& a, ll n){
-    vector<bool> b(n);
-    fi(j,0,n){
-        if(a[j] % d == 0) b[j] = true;
-        else b[j] = false;
-    }
-    return issame(b,n);
-}
-
-void solve(vi& a,ll n){
-    if(n==1){ cout<<a[0]<<endl; return; }
-
-    fi(i,0,n){
-        ll x = a[i];
-        for(ll d=1; d*d<=x; d++){
-            if(x % d == 0){
-                // try divisor d
-                if(check(d,a,n)){
-                    cout<<d<<endl;
-                    return;
-                }
-                // try divisor x/d
-                if(check(x/d,a,n)){
-                    cout<<x/d<<endl;
-                    return;
-                }
+int main()
+{
+    fast;
+    tc
+    {
+        ll n;
+        cin >> n;
+        vi v(n);
+        in(v);
+        ll odd_gcd = 0;
+        ll even_gcd = 0;
+        fi(i, 0, n)
+        {
+            if (i % 2 == 0)
+            {
+                even_gcd = gcd(even_gcd, v[i]);
+            }
+            else
+            {
+                odd_gcd = gcd(odd_gcd, v[i]);
             }
         }
-    }
-    cout<<0<<endl;
-}
-
-int main() {
-    fast;
-    tc {
-        ll n; cin>>n;
-        vi v(n); in(v);
-        solve(v,n);
+        bool flag=true;
+       for(ll i=1;i<n;i+=2){
+        if(v[i]%even_gcd==0){
+            flag=false;
+            break;
+        }
+       }
+       if(flag){
+        cout<<even_gcd<<endl;
+        continue;
+       }
+       flag=true;
+       for(ll i=0;i<n;i+=2){
+        if(v[i]%odd_gcd==0){
+            flag=false;
+            break;
+        }
+       }
+       if(flag){
+        cout<<odd_gcd<<endl;
+       }else{
+        cout<<0<<endl;
+       }
     }
 }
---
+++ Codeforces/E_Cardboard_for_Pictures.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <string>
+#include <algorithm>
+#include <numeric>
+#include <cmath>
+#include <climits>
+#include <iomanip>
+#include <utility>
+#include<unordered_set>
+#include<unistd.h>
+#include<unordered_map>
+using namespace std;
+#define ll  long long
+#define ld  long double
+#define vi  vector<ll>
+#define pi  pair<ll,ll>
+#define vpi vector<pi>
+#define pb  push_back
+#define ff  first
+#define ss  second
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) ((ll)(x).size())
+#define fi(i,s,e) for(ll i=s;i<e;i++)
+#define fd(i,s,e) for(ll i=s;i>e;i--)
+#define tc  ll t;cin>>t;while(t--)
+#define in(v) for(auto &x:v) cin>>x
+#define out(v) for(auto &x:v) cout<<x<<" "
+#define inp(v) for(auto &x:v) cin>>x.ff>>x.ss
+#define Y cout<<"YES\n"
+#define N cout<<"NO\n"
+#define nl cout<<'\n'
+#define mn3(a,b,c) min(a,min(b,c))
+#define mx3(a,b,c) max(a,max(b,c))
+#define hi(v) *max_element(all(v))
+#define lo(v) *min_element(all(v))
+#define dbg(x) cerr<<#x<<"="<<x<<'\n'
+#define fast ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+const ll MOD = 1e9+7;
+const ll INF = LLONG_MAX;
+const int Na = 2e5+5;
+
+// -----------------------------------Lets Do IT---------------------------------------------------------------
+ll findSum(vi& a,ll n){
+    ll ans=0;
+    fi(i,0,n){
+        ans+=a[i];
+    }
+    return ans;
+}
+ll findSqSum(vi& a,ll n){
+    ll ans=0;
+    fi(i,0,n){
+        ll x=a[i]*a[i];
+        ans+=x;
+    }
+    return ans;
+}
+
+int main() {
+    fast;
+    tc {
+        ll n,c; cin>>n>>c;
+        vi v(n); in(v);
+        ll suq=findSum(v,n);
+        ll ssq=(findSqSum(v,n)-c);
+        ll d=((suq*suq)-(4*n*ssq));
+        ll res1=((-suq+sqrt(d))/(2*n));
+        cout<<res1<<endl;
+    }
+}
