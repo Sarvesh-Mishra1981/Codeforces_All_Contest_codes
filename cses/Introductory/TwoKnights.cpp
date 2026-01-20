4fe996f61de53f56f3e300eeb0118e15c1b66143
Sarvesh Mishra <191090264+Sarvesh-Mishra1981@users.noreply.github.com>
2026-01-20 13:09:43 +0530

    20JAN CSES DONE

---
+++ cses/Introductory/TwoKnights.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <string>
+#include <algorithm>
+#include <numeric>
+#include <cmath>
+#include <climits>
+#include <iomanip>
+#include <utility>
+#include<unordered_set>
+#include<unistd.h>
+#include<unordered_map>
+using namespace std;
+#define ll  long long
+#define ld  long double
+#define vi  vector<ll>
+#define pi  pair<ll,ll>
+#define vpi vector<pi>
+#define pb  push_back
+#define ff  first
+#define ss  second
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) ((ll)(x).size())
+#define fi(i,s,e) for(ll i=s;i<e;i++)
+#define fd(i,s,e) for(ll i=s;i>e;i--)
+#define tc  ll t;cin>>t;while(t--)
+#define in(v) for(auto &x:v) cin>>x
+#define out(v) for(auto &x:v) cout<<x<<" "
+#define inp(v) for(auto &x:v) cin>>x.ff>>x.ss
+#define Y cout<<"YES\n"
+#define N cout<<"NO\n"
+#define nl cout<<'\n'
+#define mn3(a,b,c) min(a,min(b,c))
+#define mx3(a,b,c) max(a,max(b,c))
+#define hi(v) *max_element(all(v))
+#define lo(v) *min_element(all(v))
+#define dbg(x) cerr<<#x<<"="<<x<<'\n'
+#define fast ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+const ll MOD = 1e9+7;
+const ll INF = LLONG_MAX;
+const int Na = 2e5+5;
+
+// -----------------------------------Lets Do IT---------------------------------------------------------------
+ll solve(ll n){
+    //number of ways to place the 2 knights 1*(k^2-1)*k^2 coz space is k^2 and the ek jgh rakha then k2-1 left hinge 
+    // and we have done/2 coz there will be repeating spaces
+    ll numOfSpaces=(n*n)*((n*n)-1)/2;
+    ll numOfAttack=4*(n-1)*(n-2);
+    return (numOfSpaces-numOfAttack);
+}
+
+int main() {
+    fast;
+
+        ll n; cin>>n;
+        fi(i,1,n+1){
+            cout<<solve(i)<<endl;
+        }
+}
