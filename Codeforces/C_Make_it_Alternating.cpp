8b34897e3a4d339c63ae036fa6aaa97df28519e8
Sarvesh Mishra <191090264+Sarvesh-Mishra1981@users.noreply.github.com>
2026-02-07 18:58:34 +0530

    7FEB2026

---
+++ Codeforces/C_Make_it_Alternating.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <string>
+#include <algorithm>
+#include <numeric>
+#include <cmath>
+#include <climits>
+#include <iomanip>
+#include <utility>
+#include<unordered_set>
+#include<unistd.h>
+#include<unordered_map>
+using namespace std;
+#define ll  long long
+#define ld  long double
+#define vi  vector<ll>
+#define pi  pair<ll,ll>
+#define vpi vector<pi>
+#define pb  push_back
+#define ff  first
+#define ss  second
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) ((ll)(x).size())
+#define fi(i,s,e) for(ll i=s;i<e;i++)
+#define fd(i,s,e) for(ll i=s;i>e;i--)
+#define tc  ll t;cin>>t;while(t--)
+#define in(v) for(auto &x:v) cin>>x
+#define out(v) for(auto &x:v) cout<<x<<" "
+#define inp(v) for(auto &x:v) cin>>x.ff>>x.ss
+#define Y cout<<"YES\n"
+#define N cout<<"NO\n"
+#define nl cout<<'\n'
+#define mn3(a,b,c) min(a,min(b,c))
+#define mx3(a,b,c) max(a,max(b,c))
+#define hi(v) *max_element(all(v))
+#define lo(v) *min_element(all(v))
+#define dbg(x) cerr<<#x<<"="<<x<<'\n'
+#define fast ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+const ll MOD = 1e9+7;
+const ll INF = LLONG_MAX;
+const int Na = 2e5+5;
+
+// -----------------------------------Lets Do IT---------------------------------------------------------------
+ll minnum(string& s){
+    if(s.length()==1) return 0;
+    ll n=s.length();
+    ll i=0,j=1;
+    ll ans=0;
+    while(j<n){
+        if(s[i]!=s[j]){
+            i=j;
+            j++;
+        }else{
+            ans++;
+            j++;
+        }
+    }
+    return ans;
+}
+ll solve(string& s,ll m){
+    ll n=s.length();
+    ll val=1;
+    fi(i,0,n-1){
+        if(s[i]==s[i+1]){
+            val++;
+        }
+    }
+    ll ans=1;
+    while(m--){
+        ans*=val;
+        val--;
+    }
+    return ans;
+}
+int main() {
+    fast;
+    tc {
+       string s; cin>>s;
+       ll val=minnum(s);
+       ll ans=solve(s,val);
+       cout<<val<<" "<<ans<<endl;
+    }
+}
---
+++ Codeforces/H_Maximal_AND.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <string>
+#include <algorithm>
+#include <numeric>
+#include <cmath>
+#include <climits>
+#include <iomanip>
+#include <utility>
+#include<unordered_set>
+#include<unistd.h>
+#include<unordered_map>
+using namespace std;
+#define ll  long long
+#define ld  long double
+#define vi  vector<ll>
+#define pi  pair<ll,ll>
+#define vpi vector<pi>
+#define pb  push_back
+#define ff  first
+#define ss  second
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) ((ll)(x).size())
+#define fi(i,s,e) for(ll i=s;i<e;i++)
+#define fd(i,s,e) for(ll i=s;i>e;i--)
+#define tc  ll t;cin>>t;while(t--)
+#define in(v) for(auto &x:v) cin>>x
+#define out(v) for(auto &x:v) cout<<x<<" "
+#define inp(v) for(auto &x:v) cin>>x.ff>>x.ss
+#define Y cout<<"YES\n"
+#define N cout<<"NO\n"
+#define nl cout<<'\n'
+#define mn3(a,b,c) min(a,min(b,c))
+#define mx3(a,b,c) max(a,max(b,c))
+#define hi(v) *max_element(all(v))
+#define lo(v) *min_element(all(v))
+#define dbg(x) cerr<<#x<<"="<<x<<'\n'
+#define fast ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+const ll MOD = 1e9+7;
+const ll INF = LLONG_MAX;
+const int Na = 2e5+5;
+
+// -----------------------------------Lets Do IT---------------------------------------------------------------
+ll solve(ll n, ll k, vi& a){
+    vi bits(31, 0);
+
+    for(ll i = 0; i < n; i++){
+        for(ll j = 0; j <= 30; j++){
+            if(a[i] & (1LL << j)){
+                bits[j]++;
+            }
+        }
+    }
+    ll ans = 0;
+    for(ll j = 30; j >= 0; j--){
+        ll need = n - bits[j];   
+        if(k >= need){
+            k -= need;
+            ans |= (1LL << j);
+        }
+    }
+    return ans;
+}
+
+int main() {
+    fast;
+    tc {
+        ll n,k; cin>>n>>k;
+        vi a(n); in(a);
+        cout<<solve(n,k,a)<<endl;
+    }
+}
