da7daf4311f1a443c05a48fd3b12a9bc433d74a6
Sarvesh Mishra <191090264+Sarvesh-Mishra1981@users.noreply.github.com>
2026-02-09 23:10:56 +0530

    9Feb202

---
+++ B_Alyona_and_a_Narrow_Fridge.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <string>
+#include <algorithm>
+#include <numeric>
+#include <cmath>
+#include <climits>
+#include <iomanip>
+#include <utility>
+#include<unordered_set>
+#include<unistd.h>
+#include<unordered_map>
+using namespace std;
+#define ll  long long
+#define ld  long double
+#define vi  vector<ll>
+#define pi  pair<ll,ll>
+#define vpi vector<pi>
+#define pb  push_back
+#define ff  first
+#define ss  second
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) ((ll)(x).size())
+#define fi(i,s,e) for(ll i=s;i<e;i++)
+#define fd(i,s,e) for(ll i=s;i>e;i--)
+#define tc  ll t;cin>>t;while(t--)
+#define in(v) for(auto &x:v) cin>>x
+#define out(v) for(auto &x:v) cout<<x<<" "
+#define inp(v) for(auto &x:v) cin>>x.ff>>x.ss
+#define Y cout<<"YES\n"
+#define N cout<<"NO\n"
+#define nl cout<<'\n'
+#define mn3(a,b,c) min(a,min(b,c))
+#define mx3(a,b,c) max(a,max(b,c))
+#define hi(v) *max_element(all(v))
+#define lo(v) *min_element(all(v))
+#define dbg(x) cerr<<#x<<"="<<x<<'\n'
+#define fast ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+const ll MOD = 1e9+7;
+const ll INF = LLONG_MAX;
+const int Na = 2e5+5;
+
+// -----------------------------------Lets Do IT---------------------------------------------------------------
+ll solve(vi& a,ll n,ll k){
+    ll high=n-1;
+    ll low=0;
+    ll ans=0;
+    while(low<=high){
+        ll mid=(high+low)/2;
+        vi temp;
+        fi(i,0,mid+1){
+            temp.push_back(a[i]);
+        }
+        sort(temp.begin(),temp.end());
+        ll p=mid,curr=0;
+        while(p>=0){
+            curr+=temp[p];
+            p-=2;
+            if(curr>k){
+                break;
+            }
+        }
+        if(curr<=k){
+            ans=mid;
+            low=mid+1;
+        }else{
+            high=mid-1;
+        }
+    }
+    return ans+1;
+}
+
+int main() {
+    fast;
+        ll n,k; cin>>n>>k;
+        vi a(n); in(a);
+        cout<<solve(a,n,k)<<endl;
+}
