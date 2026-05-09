779773fc276763355d09bf7ea77cff575b1a2e24
Sarvesh Mishra <191090264+Sarvesh-Mishra1981@users.noreply.github.com>
2026-05-09 15:44:00 +0530

    9MAY2026

---
+++ Codeforces/A_Regular_Bracket_Sequences.cpp
@@ -0,0 +1,102 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <string>
+#include <algorithm>
+#include <numeric>
+#include <cmath>
+#include <climits>
+#include <iomanip>
+#include <utility>
+#include<unordered_set>
+#include<unistd.h>
+#include<unordered_map>
+using namespace std;
+#define ll  long long
+#define ld  long double
+#define vi  vector<ll>
+#define vvi vector<vector<ll>>
+#define pi  pair<ll,ll>
+#define vpi vector<pi>
+#define pb  push_back
+#define ff  first
+#define ss  second
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) ((ll)(x).size())
+#define fi(i,s,e) for(ll i=s;i<e;i++)
+#define fd(i,s,e) for(ll i=s;i>e;i--)
+#define tc  ll t;cin>>t;while(t--)
+#define in(v) for(auto &x:v) cin>>x
+#define out(v) for(auto &x:v) cout<<x<<" "
+#define inp(v) for(auto &x:v) cin>>x.ff>>x.ss
+#define Y cout<<"YES\n"
+#define N cout<<"NO\n"
+#define nl cout<<'\n'
+#define mn3(a,b,c) min(a,min(b,c))
+#define mx3(a,b,c) max(a,max(b,c))
+#define hi(v) *max_element(all(v))
+#define lo(v) *min_element(all(v))
+#define dbg(x) cerr<<#x<<"="<<x<<'\n'
+#define fast ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+const ll MOD = 1e9+7;
+const ll INF = LLONG_MAX;
+const int Na = 2e5+5;
+
+// -----------------------------------Lets Do IT---------------------------------------------------------------
+ll cnt = 0;
+
+void solve(string &curr, ll open, ll close, ll n){
+
+    // stop after n outputs
+    if(cnt == n) return;
+
+    // valid string formed
+    if(open == 0 && close == 0){
+        cout << curr << '\n';
+        cnt++;
+        return;
+    }
+
+    // add '('
+    if(open > 0){
+        curr.push_back('(');
+
+        solve(curr, open - 1, close, n);
+
+        curr.pop_back();
+    }
+
+    // add ')'
+    if(close > open){
+        curr.push_back(')');
+
+        solve(curr, open, close - 1, n);
+
+        curr.pop_back();
+    }
+}
+
+int main(){
+
+    fast;
+
+    ll t;
+    cin >> t;
+
+    while(t--){
+
+        ll n;
+        cin >> n;
+
+        cnt = 0;
+
+        string curr = "";
+
+        solve(curr, n, n, n);
+    }
+}
